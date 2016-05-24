2a6d614b453c545bce4c8a8b0ef9d7548554c74b
Alessandro Maroso <alessandro.maroso@gmail.com>
2016-05-24 16:59:02 +0200

    Test serving static log file.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ winston.remove(winston.transports.Console);
 winston.add(winston.transports.Console, { level: 'info' });
 
 var statusHandler = require('./routes/status');
-
+var logHandler = require('./routes/log');
 var telegramHandler = require('./routes/telegram');
 
 var app = express();
@@ -17,6 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/status', statusHandler);
 app.post('/telegramBot', telegramHandler);
+app.use(express.static());
 
 var server = app.listen(config.SERVER_PORT, function () {
   var host = server.address().address;
