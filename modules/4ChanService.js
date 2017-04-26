eed4402b998b1dc2500aa97bd3243171ffae271e
Alessandro Maroso <alessandro.maroso@rawfish.it>
2017-04-26 15:58:07 +0200

    Removed deprecated functions.

--- modules/4ChanService.js
+++ modules/4ChanService.js
@@ -88,50 +88,6 @@ function extractPosts(body) {
   return posts;
 }
 
-function extractRandomFileNames(body, count) {
-  var filenames = [];
-  for (var i = 0; i < count; i++) {
-    var filename = extractRandomFileName(body);
-    if (filename === undefined) {
-      return undefined;
-    }
-    if (filenames.indexOf(filename) < 0) {
-      filenames.push(filename);
-    }
-  }
-  return filenames;
-}
-
-function extractRandomFileName(body) {
-  if (!body.hasOwnProperty('threads')) {
-    return undefined;
-  }
-  if (Object.prototype.toString.call(body.threads) !== '[object Array]') {
-    return undefined;
-  }
-  var validThreads = body.threads.filter(isValidThread);
-  if (validThreads.length === 0) {
-    return undefined;
-  }
-  var posts = [];
-  var threadsCount = validThreads.length;
-  for (var i = 0; i < threadsCount; i++) {
-      var currentThread = validThreads[i];
-      var validPosts = currentThread.posts.filter(isValidPost);
-      posts = posts.concat(validPosts);
-  }
-  if (posts.length === 0) {
-    return undefined;
-  }
-  var rnd = Math.random();
-  rnd = rnd * posts.length;
-  rnd = Math.floor(rnd);
-  var randomPost = posts[rnd];
-  var fileName = randomPost.tim;
-  var fileExtension = randomPost.ext;
-  return fileName + fileExtension;
-}
-
 function isValidThread(element, index, array) {
   if (element.hasOwnProperty('posts')) {
     if (Object.prototype.toString.call( element.posts ) === '[object Array]') {
