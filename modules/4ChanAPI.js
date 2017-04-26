f8374636e8078fcc019a83bd13fa85fc2ccd9afc
Alessandro Maroso <alessandro.maroso@rawfish.it>
2017-04-26 17:27:02 +0200

    Multiple pages download test.

--- modules/4ChanAPI.js
+++ modules/4ChanAPI.js
@@ -1,5 +1,6 @@
 var config = require('../config');
 var request = require('request');
+var async = require('async');
 var fs = require('fs');
 
 var requestOptions = {
@@ -8,28 +9,35 @@ var requestOptions = {
 		'if-modified-since': (new Date()).toUTCString()
 	}
 };
-
 var chanInterface = {};
 
 chanInterface.downloadJSONForBoard = function(board, callback) {
-	var randomPageNumber = Math.floor(Math.random() * 10) + 1;
-	console.log(board);
-	if (board == "/f") {
-		randomPageNumber = 1;
-	}
-  var requestUrl = config.CHAN_BASE_URL + board + "/" + randomPageNumber + ".json";
-  request(requestUrl, requestOptions, function (err, res, body) {
-    if (err) {
-      return callback(err);
-    } else if (res.statusCode == 200) {
-      return callback(null, body);
+  var baseRequestUrl = config.CHAN_BASE_URL + board + "/";
+  var indices = [0,1,2,3,4,5];
+  var urls = indices.map(function(index) { return baseRequestUrl+index+".json"; });
+  async.map(urls, fetch, function(err, results){
+    if (err){
+       callback(err);
     } else {
-      return callback(new Error("Unable to download JSON. Code " + res.statusCode));
+        var threads = results.reduce(function(accumulator, currentValue, currentIndex, array) {
+            return accumulator.concat(currentValue.threads);
+        }, []);
+        return {'threads': threads}
     }
-  }).on('error', function(err) {
-		return callback(err);
-	});
+  });
 };
+
+function fetch(url, cb) {
+    request(file, requestOptions, function(err, response, body) {
+        if (err) {
+            cb(err);
+        } else if (response.statusCode == 200) {
+            cb(null, body);
+        } else {
+            return callback(new Error("Unable to download JSON. Code " + res.statusCode));
+        }
+    });
+}
 
 chanInterface.downloadMedia = function(name, board, localPath, callback) {
   var requestUrl = config.CHAN_IMAGE_BASE_URL + board + "/" + name;
